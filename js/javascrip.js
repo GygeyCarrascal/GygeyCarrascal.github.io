d68145238d8cccdda7865941f71915627a63b83d
Ginori Carrascal <Gcarrascals15_2@unc.edu.pe>
2020-10-04 15:18:07 -0500

    ok

--- js/javascrip.js
+++ js/javascrip.js
@@ -1,7 +1,11 @@
-$(document).ready(function(){
-    $('.slider').slider({
-      full_width: false,
-      interval:5000,
-      transition:800,
-    });
-  });
+ // Call carousel manually
+ $('#myCarouselCustom').carousel();
+            
+ // Go to the previous item
+ $("#prevBtn").click(function(){
+     $("#myCarouselCustom").carousel("prev");
+ });
+ // Go to the previous item
+ $("#nextBtn").click(function(){
+     $("#myCarouselCustom").carousel("next");
+ });
